830f98f0109b477648e397d35525d2ca94e4e271
skhandorin <hsatig@mail.ru>
2017-05-19 19:44:02 +0500

    Startup and Middleware - Using IApplicationBuilder

---
+++ OdeToFood/Greeter.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OdeToFood
+{
+
+    public interface IGreeter
+    {
+        string GetGreeting();
+    }
+
+    public class Greeter : IGreeter
+    {
+        private string _greeting;
+        private int _nothing;
+
+        public Greeter(IConfiguration configuration)
+        {
+            _greeting = configuration["Greeting"];
+            _nothing = 0;
+        }
+
+        public string GetGreeting()
+        {
+            return _greeting;
+        }
+    }
+}
--- OdeToFood/Startup.cs
+++ OdeToFood/Startup.cs
@@ -7,19 +7,34 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
 
 namespace OdeToFood
 {
     public class Startup
     {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                            .SetBasePath(env.ContentRootPath)
+                            .AddJsonFile("appsettings.json")
+                            .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+        }
+
+        public IConfiguration Configuration { get; set; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton(Configuration);
+            services.AddSingleton<IGreeter, Greeter>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IGreeter greeter)
         {
             loggerFactory.AddConsole();
 
@@ -28,9 +43,12 @@ namespace OdeToFood
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseWelcomePage();
+
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                var message = greeter.GetGreeting();
+                await context.Response.WriteAsync(message);
             });
         }
     }
